b18cc76ec22959663776a9d0641e66909b250717
Seunghun Jeong <zldzmfoq12@users.noreply.github.com>
2020-02-14 15:22:07 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -59,7 +59,7 @@ def write_metadata(metadata, out_dir):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_dir', default=os.path.expanduser('~/jeewoo/GST-Tacotron/'))
+    parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron/GST-Tacotron/'))
     parser.add_argument('--output', default='training')
     parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'bible', 'kss', 'bts'])
     parser.add_argument('--num_workers', type=int, default=cpu_count())
